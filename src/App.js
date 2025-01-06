0f238147219e8ad9ddb65a6c557edb8d464d51d7
Abhishek Moreshiya <abhishekmoreshiya3@gmail.com>
2025-01-06 20:55:21 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p className='font-bold text-green-700'>
+         Hello this is create react app.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
